b6c82c1d754a28dd93c1b506a24dbe91f118d670
Brandon Graupner <brandon.graupner@gmail.com>
2021-07-06 23:35:17 +1000

    v1 converts text in file to morsecode

--- main.py
+++ main.py
@@ -5,10 +5,27 @@ def read_letters(file_name):
     output = []
     while i < len(contents):
         letter = contents[i]
-        output.append()
+        print(letter)
+        output.append(morsecode(letter))
         i += 1
+    return output
 
-def morsecode(letter):
+def morsecode(char):
     morse = ["*-", "-***", "-*-*", "-**", "*", "**-*", "--*", "****", "**", "*---", "-*-", "*-**", "--", "-*", "---", "*--*", "--*-", "*-*", "***", "-", "**-", "***-", "*--", "-**-", "-*--", "--**"]
-    numbers = ["-----", "*----", "**---", "***--", "****-", "*****", "-****", "--***", "---**", "----*", "-----"]
-    
+    morse_numbers = ["-----", "*----", "**---", "***--", "****-", "*****", "-****", "--***", "---**", "----*", "-----"]
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    i = 0
+    if char.isdigit():
+        while i < len(numbers):
+            if char == numbers[i]:
+                return morse_numbers[i]
+            i+=1
+    else:
+        while i < len(letters):
+            if char == letters[i]:
+                return morse[i]
+            i+=1
+
+
+print(read_letters("file.txt"))
